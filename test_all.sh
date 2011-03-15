a1968c5357f2212e3839382314672a520efd3bf9
Alexandr Petrov <firexel@gmail.com>
2011-03-15 22:18:51 +0300

    modifyed test_all.sh to use ./add_corpus_to model instead of dspam_train
    Signed-off-by: Alexandr Petrov <firexel@gmail.com>

--- test_all.sh
+++ test_all.sh
@@ -3,9 +3,10 @@
 PARTS_COUNT=5
 CORPUS="corpus"
 USERNAME="firexel"
-DSPAM_TRAIN=dspam_train
+DSPAM_TRAIN="./add_corpus_to_model.py"
 DSPAM_PROFILE=/home/firexel/dspam/var/dspam/data/
-
+GENERATE_MODEL="./generate_model.py"
+TESTING_TYPE=$1
 
 function split_dir {
    dir=$1
@@ -28,14 +29,19 @@ for CONTROL in `seq 0 $MAX_PART_NUM`; do
     for TRAIN in `seq 0 $MAX_PART_NUM`; do
         if [ $CONTROL != $TRAIN ]; then
             echo "training on part $TRAIN.."
-            $DSPAM_TRAIN $USERNAME $TRAIN/$CORPUS/spam $TRAIN/$CORPUS/ham
+            $DSPAM_TRAIN $USERNAME $TRAIN/$CORPUS/spam $TRAIN/$CORPUS/ham > /dev/null
         fi
     done;
     echo "checking on part $CONTROL.."
     ./dspam_test.sh $CONTROL/$CORPUS/spam | ./analyze.py spam
     ./dspam_test.sh $CONTROL/$CORPUS/ham | ./analyze.py ham
+    if [ "$TESTING_TYPE" -eq "svm" ]; then
+        echo "generating model..."
+        $GENERATE_MODEL
+    fi
 
     echo "removing profile.."
     rm -rf $DSPAM_PROFILE/$USERNAME
+    rm -rf $DSPAM_PROFILE/../raw_model  $DSPAM_PROFILE/../model $DSPAM_PROFILE/../model_index
 done;
 
